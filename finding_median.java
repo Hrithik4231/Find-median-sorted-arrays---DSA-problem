c9d9716e483bcb9384a1cf60598917bd8f4cac39
Hrithik Raj chauhan <hrithikrajchauhan100@gmail.com>
2026-02-06 06:12:45 +0530

    Implement FindMedianSortedArrays method

--- finding_median.java
+++ finding_median.java
@@ -1,59 +1,34 @@
-// import java.util.HashMap;
-// // import java.util.Map;
-// import java.util.HashSet;
-
-// class Main{
-    
-//     public static void main(String[] args){
-
-//         int[] arr = {1, 2, 3, 2, 1, 4, 5, 3, 6};
-//         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
-
-//         for(int num : arr){
-//             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-//         }
-//         System.out.println("Element Frequencies:");
-
-//         for (int key : frequencyMap.keySet()) {
-//             System.out.println(key + " = " + frequencyMap.get(key));
-//             if(frequencyMap.get(key) == 1){
-//                 System.out.println("Found first non-repeating element: " + key);
-//                 break;
-//             }
-//         }
-
-//         HashSet<Integer> removeduplicate = new HashSet<>();
-//             for(int num : arr){
-//                 removeduplicate.add(num);
-//                 System.out.println("Set after removing duplicates:" + removeduplicate);
-//             }
-    
-//     }
-// }
-
-// class Student {
-//     int marks;
-
-//     Student(int marks) {
-//         this.marks = marks;
-//     }
-
-//     boolean isPass() {
-//         return marks >= 35;
-//     }
-// }
-
-// class Main {
-//     public static void main(String[] args) {
-//         Student s = new Student(70);
-//         System.out.println(s.isPass());
-//     }
-// }
-
-
-class Main{
-    public static void main(String[] args){
-        int[] nums = {1,3,5,7,9};
-        System.out.println(nums.length);
+public class finding_median {
+    public int[] FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merged = new int[m+n];
+        int i=0, j=0,k=0;
+        while (i<m && j<n) {
+            if (nums1[i] <= nums2[j]) {
+                merged[k] = nums1[i];
+                i++;
+            } else {
+                merged[k] = nums2[j];
+                j++;
+            }
+            k++;   
+        }
+        while (i<m) {
+            merged[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while (j<n) {
+            merged[k] = nums2[j];
+            j++;
+            k++;
+        }
+        double median;
+        int totalLength = merged.length;
+        if (totalLength % 2 == 0) {
+            median = (merged[totalLength/2 - 1] + merged[totalLength/2]) / 2.0;
+        } else {
+            median = merged[totalLength/2];
+        }
+        return new int[]{(int)median};
     }
 }
